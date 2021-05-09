de094e33d77ed2d8487da58271241ddd0ff9999f
amamenko <amamenko@hotmail.com>
2021-05-08 22:48:16 -0400

    Integrated Contentful esthetician and price resources

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import InputSection from "./components/InputSection";
-import "./App.scss";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
+import axios from "axios";
+import "./App.scss";
 
 const App = () => {
   const [tips, changeTips] = useState(0);
@@ -10,101 +11,112 @@ const App = () => {
   const [numberOfAddOns, changeNumberOfAddOns] = useState([]);
   const [totalFacialPrice, changeTotalFacialPrice] = useState(0);
   const [totalAddOnPrice, changeTotalAddOnPrice] = useState(0);
+  const [estheticianResults, changeEstheticianResults] = useState("");
 
-  const facialPrices = [
-    {
-      name: "Facial",
-      price: 23,
-    },
-    {
-      name: "Dermaplaning",
-      price: 34,
-    },
-    {
-      name: "Chem Peel",
-      price: 30,
-    },
-    {
-      name: "Bacial",
-      price: 26,
-    },
-    {
-      name: "Microneedling",
-      price: 50,
-    },
-    {
-      name: "CBD Facial",
-      price: 25,
-    },
-    {
-      name: "Hydra Jet Peel",
-      price: 60,
-    },
-  ];
+  // Current Esthetician Price Data
+  const [currentEsthetician, changeCurrentEsthetician] = useState("");
+  const [currentFacialPrices, changeCurrentFacialPrices] = useState("");
+  const [currentAddOnPrices, changeCurrentAddOnPrices] = useState("");
+  const [currentExtrasPrices, changeCurrentExtrasPrices] = useState("");
 
   useEffect(() => {
-    changeNumberOfMainServices(new Array(facialPrices.length).fill(""));
-  }, [facialPrices.length]);
-
-  const addOnPrices = [
-    {
-      name: "Extra Massage Time",
-      price: 5,
-    },
-    {
-      name: "Extra Extraction Time",
-      price: 3,
-    },
-    {
-      name: "Party Prep Mini Peel",
-      price: 5,
-    },
-    {
-      name: "Microdermabrasion",
-      price: 4,
-    },
-    {
-      name: "Beard Deep Cleanse",
-      price: 6,
-    },
-    {
-      name: "Dermarolling",
-      price: 2,
-    },
-    {
-      name: "Gua Sha",
-      price: 8,
-    },
-    {
-      name: "Hydroboost",
-      price: 2,
-    },
-    {
-      name: "LED Extra Time",
-      price: 2,
-    },
-    {
-      name: "Microcurrent",
-      price: 4,
-    },
-  ];
+    if (currentFacialPrices) {
+      changeNumberOfMainServices(
+        new Array(currentFacialPrices.length).fill("")
+      );
+    }
+  }, [currentFacialPrices]);
 
   useEffect(() => {
-    changeNumberOfAddOns(new Array(addOnPrices.length).fill(""));
-  }, [addOnPrices.length]);
+    const estheticianQuery = `
+      query {
+        estheticianCollection {
+          items {
+            name
+            facials 
+            addOns
+            extras
+          }
+        }
+      }
+    `;
+
+    axios({
+      url: `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}`,
+      method: "post",
+      headers: {
+        "Content-Type": "Application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`,
+      },
+      data: {
+        query: estheticianQuery,
+      },
+    })
+      .then((res) => res.data)
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        if (data) {
+          if (data.estheticianCollection) {
+            if (data.estheticianCollection.items) {
+              changeEstheticianResults(data.estheticianCollection.items);
+
+              if (data.estheticianCollection.items[0].name) {
+                changeCurrentEsthetician(
+                  data.estheticianCollection.items.sort((a, b) =>
+                    a.name.localeCompare(b.name)
+                  )[0].name
+                );
+              }
+            }
+          }
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    if (estheticianResults) {
+      changeTips(0);
+      changeProducts(0);
+
+      const filteredEsthetician = estheticianResults.filter(
+        (esthetician) => esthetician.name === currentEsthetician
+      );
+
+      if (filteredEsthetician) {
+        if (filteredEsthetician[0]) {
+          changeCurrentFacialPrices(filteredEsthetician[0].facials);
+          changeCurrentAddOnPrices(filteredEsthetician[0].addOns);
+          changeCurrentExtrasPrices(filteredEsthetician[0].extras);
+        }
+      }
+    }
+  }, [currentEsthetician, estheticianResults]);
+
+  useEffect(() => {
+    if (currentAddOnPrices) {
+      changeNumberOfAddOns(new Array(currentAddOnPrices.length).fill(""));
+    }
+  }, [currentAddOnPrices]);
 
   return (
     <>
-      <NavigationBar />
+      <NavigationBar
+        currentEsthetician={currentEsthetician}
+        changeCurrentEsthetician={changeCurrentEsthetician}
+        estheticianResults={estheticianResults}
+      />
       <div className="app_container">
         <div>
           <InputSection
             sectionTitle={"Main Treatments"}
-            sectionArr={facialPrices}
+            sectionArr={currentFacialPrices}
             numberOfMainServices={numberOfMainServices}
             changeNumberOfMainServices={changeNumberOfMainServices}
-            facialPrices={facialPrices}
-            addOnPrices={addOnPrices}
+            facialPrices={currentFacialPrices}
+            addOnPrices={currentAddOnPrices}
             totalFacialPrice={totalFacialPrice}
             changeTotalFacialPrice={changeTotalFacialPrice}
             totalAddOnPrice={totalAddOnPrice}
@@ -114,21 +126,18 @@ const App = () => {
         <div>
           <InputSection
             sectionTitle={"Add Ons"}
-            sectionArr={addOnPrices}
+            sectionArr={currentAddOnPrices}
             numberOfAddOns={numberOfAddOns}
             changeNumberOfAddOns={changeNumberOfAddOns}
-            facialPrices={facialPrices}
-            addOnPrices={addOnPrices}
+            facialPrices={currentFacialPrices}
+            addOnPrices={currentAddOnPrices}
             changeTotalAddOnPrice={changeTotalAddOnPrice}
           />
         </div>
         <div>
           <InputSection
             sectionTitle={"Extras"}
-            sectionArr={[
-              { name: "Tips", percent: 3 },
-              { name: "Products", percent: 10 },
-            ]}
+            sectionArr={currentExtrasPrices}
             tips={tips}
             changeTips={changeTips}
             products={products}
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,18 +2,49 @@ import React from "react";
 import Select from "react-select";
 import "./NavigationBar.scss";
 
-const NavigationBar = () => {
-  const options = [
-    { value: "Nayla", label: "Nayla" },
-    { value: "Nicole", label: "Nicole" },
-    { value: "Erin", label: "Erin" },
-    { value: "Jenna", label: "Jenna" },
-  ];
+const NavigationBar = (props) => {
+  const {
+    currentEsthetician,
+    changeCurrentEsthetician,
+    estheticianResults,
+  } = props;
+
+  const options =
+    estheticianResults &&
+    estheticianResults
+      .map((item) => {
+        return {
+          value: item.name,
+          label: item.name,
+        };
+      })
+      .sort((a, b) => a.value.localeCompare(b.value));
+
+  const customStyles = {
+    option: (provided, state) => {
+      return {
+        ...provided,
+        background:
+          state.isFocused && state.data.value !== currentEsthetician
+            ? "rgba(178,212,255, 0.4)"
+            : state.data.value === currentEsthetician
+            ? "rgb(178,212,255)"
+            : "#fff",
+      };
+    },
+  };
 
   return (
     <div className="navigation_bar">
       <div className="logo_container">Glow Labs Payroll</div>
-      <Select className="esthetician_select_container" options={options} />
+      <Select
+        styles={customStyles}
+        value={currentEsthetician}
+        placeholder={currentEsthetician}
+        className="esthetician_select_container"
+        options={options}
+        onChange={(item) => changeCurrentEsthetician(item.value)}
+      />
     </div>
   );
 };
